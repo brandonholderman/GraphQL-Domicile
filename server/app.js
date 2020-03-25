844c02eef0511bb1f1b3c78dfc66abd7a0770835
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2020-03-25 15:33:21 -0700

    error stating GraphQLObjectType is not a constructor

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 const app = express()
 const port = 3000
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/graphql', graphqlHTTP({
-    
+    schema,
+    graphiql: true
 }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ server/schema/schema.js
@@ -0,0 +1,70 @@
+const graphql = ('graphql')
+const _ = require('lodash')
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema} = graphql
+
+// Dummy Data
+let buildings = [
+    {name: 'CR322', nodes: {id: '1'}},
+    {name: 'CR401', nodes: {id: '2'}},
+    {name: 'CR423', nodes: {id: '3'}},
+]
+
+const BuildingType = new GraphQLObjectType({
+    name: 'Building',
+    fields: () => ({
+        totalCount: {type: GraphQLInt},
+        nodes: {
+            id: {type: GraphQLString},
+            name: {type: GraphQLString}
+        },
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        building: {
+            type: BuildingType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                // code to get data from DB / other source
+                return _.find(buildings, {id: args.nodes.id})
+            }
+        }
+    }
+})
+
+
+// // Dummy Data
+// let buildings = [
+//     {id: '1', name: CR322},
+//     {id: '2', name: CR401},
+//     {id: '3', name: CR423},
+// ]
+
+// const BuildingType = new GraphQLObjectType({
+//     name: 'Building',
+//     fields: () => ({
+//         totalCount: {type: GraphQLString},
+//         id: {type: GraphQLString},
+//         name: {type: GraphQLString},
+//     })
+// })
+
+// const RootQuery = new GraphQLObjectType({
+//     name: 'RootQueryType',
+//     fields: {
+//         building: {
+//             type: BuildingType,
+//             args: {id: {type: GraphQLString}},
+//             resolve(parent, args){
+//                 // code to get data from DB / other source
+//                 _.find(buildings, {id: args.id})
+//             }
+//         }
+//     }
+// })
+
+modeule.exports = new GraphQLSchema({
+    query: RootQuery
+})
