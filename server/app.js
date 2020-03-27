104e5af1c710dd206c81a9b709db12f916eafd18
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2020-03-27 13:08:32 -0700

    server running - error with json data in graphiql

--- server/app.js
+++ server/app.js
@@ -4,8 +4,9 @@ const schema = require('./schema/schema')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use('/graphql', graphqlHTTP({
+app.get('/graphql', graphqlHTTP({
     schema,
+    graphiql: true
 }))
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,22 +1,47 @@
 const graphql = require('graphql')
 const _ = require('lodash')
-const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema} = graphql
+const {
+    GraphQLObjectType, 
+    GraphQLInt, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLList,
+} = graphql
+
 
 // Mock Data
 let buildings = [
-    {name: 'CR322', nodes: {id: '1'}},
-    {name: 'CR401', nodes: {id: '2'}},
-    {name: 'CR423', nodes: {id: '3'}},
+    {totalCount: 3, id: 1, name: 'CR322'},
+    {totalCount: 3, id: 2, name: 'CR401'},
+    {totalCount: 3, id: 3, name: 'CR513'},
 ]
 
-const BuildingType = new GraphQLObjectType({
+// let listings = [
+//     {id: 1, name: 'CR322'},
+//     {id: 2, name: 'CR401'},
+//     {id: 3, name: 'CR513'},
+// ]
+
+const Building = new GraphQLObjectType({
     name: 'Building',
     fields: () => ({
         totalCount: {type: GraphQLInt},
         nodes: {
-            id: {type: GraphQLString},
-            name: {type: GraphQLString}
-        },
+            type: new GraphQLList(Nodes),
+            resolve(parent, args){
+                console.log(parent)
+                return _.find((listings, {id: args.id}))
+            }
+        }
+    })
+})
+
+const Nodes = new GraphQLObjectType({
+    name: 'nodes',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString}
     })
 })
 
@@ -24,16 +49,38 @@ const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         building: {
-            type: BuildingType,
-            args: {id: {type: GraphQLString}},
+            type: Building,
+            args: {id: {type: GraphQLID}},
             resolve(parent, args){
                 // Gets data from DB/Source
                 return _.find(buildings, {id: args.nodes.id})
+            }
+        },
+        nodes: {
+            type: Nodes,
+            args: { id: {type: GraphQLID} },
+            resolve(parent, args){
+                return _.find(listings, {id: args.id});
             }
         }
     }
 })
 
+// Function to create uniquie user id
+// function create_UUID() {
+//     var dt = new Date().getTime();
+//     var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+//         var r = (dt + Math.random() * 16) % 16 | 0;
+//         dt = Math.floor(dt / 16);
+//         return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+//     });
+//     return uuid;
+// }
+// console.log(create_UUID());
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
 
 // Dummy Data
 // let buildings = [
@@ -64,7 +111,3 @@ const RootQuery = new GraphQLObjectType({
 //         }
 //     }
 // })
-
-module.exports = new GraphQLSchema({
-    query: RootQuery
-})
